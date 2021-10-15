818265762fc5785c5d271084bf536accf82396c5
rlaphoenix <pragma.exe@gmail.com>
2021-10-15 15:58:27 +0100

    Add Worker Class to Main UI, Method to find HDDs

--- hdlg/ui/main.py
+++ hdlg/ui/main.py
@@ -9,3 +9,27 @@ class Main(BaseWindow):
 
         self.window.actionExit.triggered.connect(self.window.close)
         self.window.actionAbout.triggered.connect(self.about)
+
+
+class MainWorker(QtCore.QObject):
+    error = QtCore.Signal(Exception)
+    finished = QtCore.Signal(int)
+    found_device = QtCore.Signal(wmi._wmi_object)
+
+    def find_hdds(self) -> None:
+        """Find Disk Drive devices using win32 api on Windows, or lsscsi on Linux."""
+        try:
+            # TODO: Filter only PS2 APA-formatted HDDs, Support Linux and macOS
+            system = platform.system()
+            if system == "Windows":
+                # noinspection PyUnresolvedReferences
+                pythoncom.CoInitialize()  # important!
+                c = wmi.WMI()
+                disk_drives = c.Win32_DiskDrive()
+                for disk_drive in sorted(disk_drives, key=lambda d: d.index, reverse=True):
+                    self.found_device.emit(disk_drive)
+                self.finished.emit(len(disk_drives))
+            else:
+                raise NotImplementedError("Device Scanning has not been implemented for %s." % system)
+        except Exception as e:
+            self.error.emit(e)
