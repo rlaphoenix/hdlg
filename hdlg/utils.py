d14dcac5e8c24d09c82f18a7e5b56d4b3930e830
rlaphoenix <pragma.exe@gmail.com>
2021-10-16 05:12:46 +0100

    feat: Add is_admin, is_frozen, and require_admin utilities

--- hdlg/utils.py
+++ hdlg/utils.py
@@ -1,9 +1,32 @@
+import ctypes
 import re
+import sys
 
 NEIGHBORING_WHITESPACE = re.compile(r"[\s]{2,}")
 CAMEL_TO_SNAKE_1 = re.compile(r"(.)([A-Z][a-z]+)")
 CAMEL_TO_SNAKE_2 = re.compile(r"([a-z0-9])([A-Z])")
 SIZE_UNITS = ["B", "KB", "MB", "GB", "TB", "PB"]
+
+
+def is_admin() -> bool:
+    """Check if running with Administrative rights."""
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except Exception as e:
+        print(e)
+        return False
+
+
+def is_frozen() -> bool:
+    """Check if running under frozen build (cx_freeze or PyInstaller)."""
+    return getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
+
+
+def require_admin() -> None:
+    """If not running with Administrative rights, request it or close."""
+    if not is_admin():
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv[is_frozen():]), None, 0)
+        sys.exit(1)
 
 
 def camel_to_snake(name: str) -> str:
